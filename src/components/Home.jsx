0be8df98ad15a420a7c3d70402b5a6ad362719f5
kevincaeyman <kevin.caeyman@gmail.com>
2023-08-30 20:57:18 +0200

    imagesd placed in the public folder for deployment

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,14 +5,13 @@ function Home() {
     <div className="home">
       <div className="text">
         <h1>
-          Hey There! <img src="public/assets/Emoji.png" className="emoji" />{" "}
-          <br />
+          Hey There! <img src="./Emoji.png" className="emoji" /> <br />
           I'm Kevin Caeyman
         </h1>
         <p>Front-End Developer, Gamer and coffee afficionado</p>
         <button>Download CV</button>
       </div>
-      <img src="public/assets/portrait.png" alt="Kevin" className="portrait" />
+      <img src="./Portrait.png" alt="Kevin" className="portrait" />
     </div>
   );
 }
